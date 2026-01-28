e093ac9bcf023623d05ce911bdd2a7471986a5d6
Juholiver <juholiver3@gmail.com>
2026-01-28 16:28:52 -0300

    adciona a rotas do login para cadastro

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'; // 1. Importe o Router
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [], // Certifique-se de que não há conflitos aqui
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  // 2. Injete o Router no construtor
+  constructor(private router: Router) {}
+
+  // 3. Crie o método de navegação
+  goToRegister() {
+    this.router.navigate(['/register']);
+  }
 }
